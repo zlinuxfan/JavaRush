1d2128ea6c36cead0c57b527e5c1d686ccfd1631
ZLinuxFan <zlinuxfan@gmail.com>
2016-07-03 23:04:48 +0300

    Added 18/05/04-05 Input/Output Stream

---
+++ test/level18/lesson05/task04/Solution.java
@@ -0,0 +1,37 @@
+package com.javarush.test.level18.lesson05.task04;
+
+/* Реверс файла
+Считать с консоли 2 имени файла: файл1, файл2.
+Записать в файл2 все байты из файл1, но в обратном порядке
+Закрыть потоки. Не использовать try-with-resources
+*/
+
+import java.io.*;
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String fileIn = reader.readLine();
+        String filOut = reader.readLine();
+        reader.close();
+
+        FileInputStream inputStream = new FileInputStream(fileIn);
+        FileOutputStream outputStream = new FileOutputStream(filOut);
+
+        if (inputStream.available() > 0) {
+            byte[] buffer = new byte[inputStream.available()];
+            byte[] reverseBuffer = new byte[inputStream.available()];
+
+            int count = inputStream.read(buffer);
+
+            for (int i = buffer.length-1, e = 0; i >= 0; i --, e ++) {
+                reverseBuffer[e] = buffer[i];
+            }
+
+            outputStream.write(reverseBuffer, 0, count);
+        }
+
+        inputStream.close();
+        outputStream.close();
+    }
+}
